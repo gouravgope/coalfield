1cc5d7d82a22b44a3154cf0c96a621803e79911e
gourav gope <gouravofficial4@gmail.com>
2024-07-30 16:44:57 +0530

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+ /* New Copy pasted code { https://github.com/adrianhajdin/project_3D_developer_portfolio/blob/main/postcss.config.cjs  }Code */
+
+// module.exports = {
+//   plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//   }
+// }
